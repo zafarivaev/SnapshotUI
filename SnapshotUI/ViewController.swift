42d9d7cadda300e96df0b4e77faa3ce2679bf414
zivaev462 <z.ivaev@mail.ru>
2020-03-31 12:08:53 +0500

    Initial Commit

---
+++ SnapshotUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotUI
+//
+//  Created by Zafar on 3/31/20.
+//  Copyright © 2020 Zafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
